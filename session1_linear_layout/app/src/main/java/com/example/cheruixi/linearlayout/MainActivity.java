fa5d4b40f672712504163dd76a43cfc699f946b2
xiang19890319@gmail.com <xiang19890319@gmail.com>
2018-01-23 16:40:49 +0800

    session2

--- session1_linear_layout/app/src/main/java/com/example/cheruixi/linearlayout/MainActivity.java
+++ session1_linear_layout/app/src/main/java/com/example/cheruixi/linearlayout/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.cheruixi.linearlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int[] imageArray = {R.drawable.shaonv1,R.drawable.shaonv2,R.drawable.shaonv3,R.drawable.shaonv4};
+    private ImageView iv;
+    private int index = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv=(ImageView)findViewById(R.id.iv1);
+        iv.setImageResource(R.drawable.shaonv1);
+    }
+
+    public void  previousImg(View view){
+        index--;
+        if(index<0){
+            index=0;
+            Toast.makeText(this, "已经是第一张", Toast.LENGTH_SHORT).show();
+        } else {
+            iv.setImageResource(imageArray[index]);
+        }
+    }
+
+    public void  nextImg(View view){
+        index++;
+        if(index>imageArray.length-1){
+            index--;
+            Toast.makeText(this, "已经是最后一张", Toast.LENGTH_SHORT).show();
+        } else {
+            iv.setImageResource(imageArray[index]);
+        }
+    }
+
+    public void clickSubmit(View view){
+        Toast.makeText(this, "jesse is here", Toast.LENGTH_SHORT).show();
     }
 }
