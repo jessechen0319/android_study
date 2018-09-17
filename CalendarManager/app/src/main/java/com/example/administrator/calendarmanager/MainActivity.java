037009afa1858fdcca5e73ca684c2967cb383d9e
JesseChen <ruixiang.chen@hpe.com>
2018-09-18 07:54:55 +0800

    commit

--- CalendarManager/app/src/main/java/com/example/administrator/calendarmanager/MainActivity.java
+++ CalendarManager/app/src/main/java/com/example/administrator/calendarmanager/MainActivity.java
@@ -6,7 +6,15 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.example.administrator.calendarmanager.model.TaskStatus;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static ArrayList<Map<String, String>> tasks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(getResources().getColor(R.color.barColor));
+
+        /*列表模板{*/
+
+        Map<String, String> task1 = new HashMap<>();
+        task1.put("title","灵修");
+        task1.put("content", "中午看完每日灵粮");
+        task1.put("status", TaskStatus.NEW);
+        tasks.add(task1);
+
+        /*}列表模板*/
     }
 
     @Override
---
+++ CalendarManager/app/src/main/java/com/example/administrator/calendarmanager/model/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.example.administrator.calendarmanager.model;
+
+public class TaskStatus {
+    public final static String NEW = "new";
+    public final static String FINISHED = "finished";
+}
