d8b345a0c27b8d84610dc4370675f889719c6d96
JesseChen <ruixiang.chen@hpe.com>
2018-09-22 12:59:59 +0800

    fetch

--- CalendarManager/app/src/main/java/com/example/administrator/calendarmanager/MainActivity.java
+++ CalendarManager/app/src/main/java/com/example/administrator/calendarmanager/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.example.administrator.calendarmanager.model.TaskStatus;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -36,12 +40,52 @@ public class MainActivity extends AppCompatActivity {
         task1.put("status", TaskStatus.NEW);
         tasks.add(task1);
 
+        Map<String, String> task2 = new HashMap<>();
+        task2.put("title","看书");
+        task2.put("content", "看完几页书");
+        task2.put("status", TaskStatus.FINISHED);
+        tasks.add(task2);
+
         /*}列表模板*/
+
+        SimpleAdapter adapter = new SimpleAdapter(MainActivity.this,
+                tasks,
+                R.layout.tasks,
+                new String[]{"title", "content"},
+                new int[]{R.id.task_title, R.id.task_desc});
+        ListView listView = findViewById(R.id.tasks);
+        listView.setAdapter(adapter);
+
+        refreshView();
+
     }
 
     @Override
     public void setContentView(View view) {
 
         super.setContentView(view);
+    }
+
+    private void refreshView(){
+        /*设置时间*/
+        SimpleDateFormat aDate=new SimpleDateFormat("yyyy-MM-dd");
+        long now=System.currentTimeMillis();
+        String formatedDate = aDate.format(now);
+        TextView nowDateLabel = findViewById(R.id.current_date);
+        nowDateLabel.setText(formatedDate);
+
+        /*设置总任务数*/
+        TextView totalTask = findViewById(R.id.totalTask_display);
+        totalTask.setText(String.valueOf(tasks.size()));
+
+        /*设置未完成任务数*/
+        int uncompleteTask = 0;
+        for (Map task : tasks){
+            if(task.get("status") == TaskStatus.NEW){
+                uncompleteTask++;
+            }
+        }
+        TextView uncompleteTasks = findViewById(R.id.uncompleteTasks);
+        uncompleteTasks.setText(String.valueOf(uncompleteTask));
     }
 }
--- ListView2/app/src/main/java/com/example/cheruixi/listview2/SimpleAdapterDemo.java
+++ ListView2/app/src/main/java/com/example/cheruixi/listview2/SimpleAdapterDemo.java
@@ -44,4 +44,6 @@ public class SimpleAdapterDemo extends AppCompatActivity {
         listView.setAdapter(adapter);
 
     }
+
+
 }
